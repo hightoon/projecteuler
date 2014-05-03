3f7b7d66bc3b3408d03cae94ed333961a57fe5f0
hightoon <haitong.chen@gmail.com>
2014-05-03 21:18:02 +0800

    update projeuler.erl

--- projeuler.erl
+++ projeuler.erl
@@ -1,5 +1,8 @@
+%% projeuler.erl
+%% solutions for problems on projecteuler.net
+
 -module(projeuler).
--export([is_prime/1, get_nth_prime/2, sum_primes_under_n/1, factorial_digit_sum/1]).
+-export([pow_dig_sum/2, is_prime/1, get_nth_prime/2, sum_primes_under_n/1, factorial_digit_sum/1, max_digit_product/3, read_file/1]).
 
 
 pow_dig_sum(Base, Pow) ->
@@ -34,6 +37,34 @@ factorial(N) -> lists:foldl(fun(X, Product) -> Product*X end, 1, [X || X <- list
 
 factorial_digit_sum(N) -> 
     lists:foldl(fun(X, Sum) -> X - 48 + Sum end, 0, integer_to_list(factorial(N))).
+
+digit_products(Digits, NumOfSerials, Pos) -> 
+    case NumOfSerials == length(Digits) - Pos + 1 of
+        true -> [lists:foldl(fun(X, P) -> X*P end, 1, [X - 48 || X <- lists:sublist(Digits, Pos, NumOfSerials)])];
+        false -> 
+          [lists:foldl(fun(X, P) -> X*P end, 1, [X - 48 || X <- lists:sublist(Digits, Pos, NumOfSerials)])] ++ 
+          digit_products(Digits, NumOfSerials, Pos+1)
+    end.
+
+max_digit_product(Digits, NumOfSerials, Pos) -> lists:max(digit_products(Digits, NumOfSerials, Pos)).
+
+read_file(Filename) ->
+    case file:open(Filename, [read]) of
+        {ok, Fd} -> sum_num_in_line(Fd, 0, file:read_line(Fd));
+        {error, Reason} -> {error, Reason}
+    end.
+
+sum_num_in_line(Fd, Sum, {ok, Data}) ->
+    sum_num_in_line(Fd, Sum + list_to_integer(lists:sublist(Data, 1, length(Data)-1)), file:read_line(Fd));
+sum_num_in_line(Fd, Sum, eof) ->
+    file:close(Fd),
+    lists:sublist(integer_to_list(Sum), 1, 10);
+sum_num_in_line(Fd, _Sum, {error, Reason}) ->
+    file:close(Fd),
+    {error, Reason}.
+
+
+
 
 
 
